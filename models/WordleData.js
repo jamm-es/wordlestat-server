89ab6023d369adfe26af5785be02e17fc648b241
James <bfcda8d7@gmail.com>
2022-01-30 19:07:33 -0500

    Initial server, update models to not include id's in subdocs

--- models/WordleData.js
+++ models/WordleData.js
@@ -5,13 +5,15 @@ const WordleData = mongoose.model('WordleData', new mongoose.Schema({
     correct: { type: Number, required: true },
     wrongPlace: { type: Number, required: true },
     wrongLetter: { type: Number, required: true },
-    total: { type: Number, required: true }
+    total: { type: Number, required: true },
+    _id: false
   }]], required: true, default: undefined },
   byRow: { type: [{
     correct: { type: Number, required: true },
     wrongPlace: { type: Number, required: true },
     wrongLetter: { type: Number, required: true },
-    total: { type: Number, required: true }
+    total: { type: Number, required: true },
+    _id: false
   }], required: true, default: undefined },
   wins: { type: [Number], required: true, default: undefined },
   total: { type: Number, required: true },
--- server.js
+++ server.js
@@ -4,3 +4,44 @@ endpoint, using the database to return results
 */
 
 const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const WordleData = require('./models/WordleData');
+const RawData = require('./models/RawData');
+
+const credentials = require('./reader-credentials.json');
+
+mongoose
+  .connect(`mongodb://${credentials.host}:${credentials.port}/${credentials.db}`, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    user: credentials.username,
+    pass: credentials.password
+  })
+  .then(() => {
+    const app = express();
+
+    app.disable('x-powered-by');
+    app.use(helmet());
+
+    app.get('/:wordleNumber', (req, res) => {
+      WordleData
+        .findOne({ wordleNumber: req.params.wordleNumber })
+        .select('-_id -__v')
+        .lean()
+        .exec((err, wordleData) => {
+          if(err) {
+            console.log(`Internal server error`);
+            console.log(err);
+            return res.status(500).json({ message: 'Internal server error' })
+          }
+
+          if(wordleData === null) return res.status(404).json({ message: 'Stats not found' });
+
+          return res.status(200).json(wordleData);
+        });
+    });
+
+    app.listen(80);
+    console.log('Listening on port 80');
+  })
